2cdfd65a3c38d9cae6322b5a11aa537072f38c76
yangjieyi <yangjieyi@coship.com>
2019-11-07 21:51:12 +0800

    1.login UI

--- app/src/main/java/com/app/squirrel/activity/LoginAct.java
+++
@@ -1,8 +0,0 @@
-package com.app.squirrel.activity;
-
-import android.support.v4.app.FragmentActivity;
-
-public class LoginAct extends FragmentActivity {
-
-
-}
---
+++ app/src/main/java/com/app/squirrel/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.app.squirrel.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.squirrel.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
