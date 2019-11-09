bd2776a855c61ab85632f3c850c5faafa5b0443c
yangjieyi <yangjieyi@coship.com>
2019-11-09 17:17:12 +0800

    1.Main UI

--- app/src/main/java/com/app/squirrel/activity/Login4NumbActivity.java
+++ app/src/main/java/com/app/squirrel/activity/Login4NumbActivity.java
@@ -1,19 +1,15 @@
 package com.app.squirrel.activity;
 
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.app.squirrel.R;
 
-public class LoginActivity extends FragmentActivity {
+public class Login4NumbActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-
+        setContentView(R.layout.activity_login4_numb);
     }
 }
